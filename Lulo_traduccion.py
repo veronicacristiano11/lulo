9125095cc5219824acbafd62c55aaccda96c9704
veronicacristiano11 <162519204+veronicacristiano11@users.noreply.github.com>
2024-03-06 19:07:25 -0500

    Add files via upload

---
+++ Lulo_traduccion.py
@@ -0,0 +1,7 @@
+def traduccion (respuesta):
+    if "hola en ingles" in respuesta :
+        print("la traduccion es Hello")
+    elif respuesta == "uno en ingles":
+        print("La traduccion es One")
+    elif respuesta == "dos en ingles":
+        print("la traduccion es Two")
---
+++ lulo_dinero.py
@@ -0,0 +1,7 @@
+def dolar_a_peso(respuesta):
+ peso = respuesta * 3915
+ print("esa cantidad en peso es ",peso)
+
+def peso_dolar(respuesta):
+ dolar = respuesta/3915
+ print("esa cantidad en dolares es ",dolar)
